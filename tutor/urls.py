8fa5c039fd553156951022cf9bf3d7dc9a688c1c
Reilman79 <42817670+Reilman79@users.noreply.github.com>
2020-02-12 14:47:26 -0500

    More Views and URLs
    also removed django-heroku

--- tutor/urls.py
+++ tutor/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('profile/', views.profile, name='profile'),
 ]
