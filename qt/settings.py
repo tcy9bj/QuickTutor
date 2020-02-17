ce721c07789838ddabacfc64b175a24cf18fd528
Reilman79 <42817670+Reilman79@users.noreply.github.com>
2020-02-17 14:30:23 -0500

    fixed duplicate path in settings.py

--- qt/settings.py
+++ qt/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tutor.apps.TutorConfig',
 
     #Google login
     'django.contrib.sites', 
