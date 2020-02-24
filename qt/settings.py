f67dad7b4e3b7fb59877c632cb6d78f48c86d5f7
tcy9bj <tcy9bj@virginia.edu>
2020-02-24 14:44:08 -0500

    Template added to Profile Page

--- qt/settings.py
+++ qt/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #Google login
-    'django.contrib.sites', 
+    'django.contrib.sites',
     'allauth',
-    'allauth.account', 
+    'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
 ]
@@ -151,4 +151,4 @@ LOGIN_REDIRECT_URL = '/tutor/profile'
 # }
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
