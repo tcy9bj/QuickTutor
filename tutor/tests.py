22fbe383b5b3e215c9a6414e5c0d633f86daf4e0
Reilman79 <42817670+Reilman79@users.noreply.github.com>
2020-03-01 17:51:10 -0500

    Added a basic test to tests.py
    Just to test Travis.

--- tutor/tests.py
+++ tutor/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+'''
+class TutorViewsTest(TestCase):
+	def test_home(self):
+		response = self.client.get(reverse('home'))
+		self.assertEqual(response.status_code, 200)
+'''
+
+class AdditionTest(TestCase):
+	def test_add(self):
+		self.assertEqual(1+1, 2)
