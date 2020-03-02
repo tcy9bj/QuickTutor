d046883a20ae29f12e5110393819aecf4b27bfdd
Reilman79 <42817670+Reilman79@users.noreply.github.com>
2020-03-02 14:27:41 -0500

    If for django-heroku

--- qt/settings.py
+++ qt/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+if 'HEROKU' in os.environ:
+    import django_heroku
+    # Activate Django-Heroku.
+    django_heroku.settings(locals(), test_runner=False)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -151,5 +154,4 @@ LOGIN_REDIRECT_URL = '/tutor/profile'
 #    }
 # }
 
-# Activate Django-Heroku.
-django_heroku.settings(locals(), test_runner=False)
+
