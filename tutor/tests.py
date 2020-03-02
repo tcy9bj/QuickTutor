3ad04e8eabb31c8d1759d314c8064ea399f02b43
Reilman79 <42817670+Reilman79@users.noreply.github.com>
2020-03-01 21:58:31 -0500

    Update tests.py

--- tutor/tests.py
+++ tutor/tests.py
@@ -1,13 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
 
-'''
-class TutorViewsTest(TestCase):
-	def test_home(self):
-		response = self.client.get(reverse('home'))
-		self.assertEqual(response.status_code, 200)
-'''
-
 class AdditionTest(TestCase):
 	def test_add(self):
 		self.assertEqual(1+1, 2)
