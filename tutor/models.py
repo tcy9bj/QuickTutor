840d477c528dc887c709ede4eb3f00c1718cf53e
Reilman79 <42817670+Reilman79@users.noreply.github.com>
2020-03-29 13:17:58 -0400

    removed profile in tutors app
    removed profile model from tutors app, making it solely used in the users app.

--- tutor/models.py
+++ tutor/models.py
@@ -1,13 +1,2 @@
 from django.db import models
 
-class Profile(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    major = models.CharField(max_length=75)
-    major2 = models.CharField(max_length=75, blank=True)
-    email = models.EmailField(max_length=254)
-    description = models.TextField(blank=True)
-    active = models.BooleanField(default=False)
-
-    def __str__(self):
-        return (self.first_name + " " + self.last_name)
