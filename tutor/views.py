1697ead9b0e5ab8e2aa9f810109719b1ee3b5682
Reilman79 <42817670+Reilman79@users.noreply.github.com>
2020-03-30 17:59:08 -0400

    Added required login and html changes

--- tutor/views.py
+++ tutor/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 from users.models import Profile
 
+@login_required
 def home(request):
 	tutors = Profile.objects.filter(active=True)
 	context = {'tutors': tutors}
