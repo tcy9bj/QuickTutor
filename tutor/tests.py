f51d4bed2072e63ba86c6141c8a14336257a039f
tcy9bj <tcy9bj@virginia.edu>
2020-03-31 21:55:27 -0400

    Added Tests

--- tutor/tests.py
+++ tutor/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 
+from .models import Ask
+from .forms import RequestForm
+
 class ViewsTest(TestCase):
 	def setUp(self):
 		self.client = Client()
@@ -8,3 +11,11 @@ class ViewsTest(TestCase):
 	def test_login_view(self):
 		response = self.client.get('/tutor/login/')
 		self.assertEqual(response.status_code, 200)
+
+	def test_request_view(self):
+		response = self.client.get('/tutor/home/')
+		self.assertEqual(response.status_code, 302)
+
+	def test_request_valid(self):
+		user_request = RequestForm(data={'fName': 'John', 'lName': 'Smith', 'subject': 'CS1110', 'question': 'help me'})
+		self.assertTrue(user_request.is_valid())
