64a80a47c8c85eb51bb351e09f59f3991de875a6
Matt Hoffman <mfh5uw@virginia.edu>
2020-04-07 20:01:49 -0400

    added template for inbox

--- users/views.py
+++ users/views.py
@@ -31,3 +31,7 @@ def register(request):
 def profile_page(request, profile_id):
 	profile = get_object_or_404(Profile, pk=profile_id)
 	return render(request, 'users/profile.html', {'profile':profile})
+
+@login_required
+def inbox(request):
+	return render(request, 'users/inbox.html', {})
