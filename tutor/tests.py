f80163b05df77af0be5befcb6e2d90737bb647ec
Reilman79 <42817670+Reilman79@users.noreply.github.com>
2020-04-08 02:15:41 -0400

    Update tests.py

--- tutor/tests.py
+++ tutor/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 from .models import Ask
 from .forms import RequestForm
@@ -7,6 +8,8 @@ from .forms import RequestForm
 class ViewsTest(TestCase):
 	def setUp(self):
 		self.client = Client()
+		self.user1 = User.objects.create(username="tester1", email="tester1@gmail.com", first_name="John", last_name="Smith")
+		self.user2 = User.objects.create(username="tester2", email="tester2@gmail.com", first_name="Jane", last_name="Doe")
 
 	def test_login_view(self):
 		response = self.client.get('/tutor/login/')
@@ -17,5 +20,5 @@ class ViewsTest(TestCase):
 		self.assertEqual(response.status_code, 302)
 
 	def test_request_valid(self):
-		user_request = RequestForm(data={'fName': 'John', 'lName': 'Smith', 'subject': 'CS1110', 'question': 'help me'})
+		user_request = RequestForm(data={'sender':self.user1, 'receiver':self.user2, 'course': 'CS1110', 'question': 'help me'})
 		self.assertTrue(user_request.is_valid())
