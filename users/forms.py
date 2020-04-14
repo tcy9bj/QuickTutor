893dac715abbeb767727d712b442841eb97d6819
tcy9bj <tcy9bj@virginia.edu>
2020-04-13 22:55:50 -0400

    EditProfile
    Users can now edit profile

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,11 @@
 #Some code from: https://medium.com/@jainsahil1997/simple-google-authentication-in-django-58101a34736b
-
 from django import forms
 from .models import Profile
 from django.contrib.auth.models import User
 
 class ProfileUpdateForm(forms.ModelForm):
 	major2 = forms.CharField(label="2nd Major", required=False)
-	phone_number = forms.RegexField(regex=r'([0-9]{3}-){2}[0-9]{4}', 
+	phone_number = forms.RegexField(regex=r'([0-9]{3}-){2}[0-9]{4}',
 									help_text="Phone number must be entered in the format: 123-456-7890")
 
 	class Meta:
