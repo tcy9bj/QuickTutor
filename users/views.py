42de82d509d7220de3cd60fb37855e3405771b82
Reilman79 <42817670+Reilman79@users.noreply.github.com>
2020-04-14 02:09:15 -0400

    Fixed edit_profile
    Also updated profile_page.html to be remove some bugs

--- users/views.py
+++ users/views.py
@@ -128,11 +128,11 @@ def edit_profile(request, profile_id):
 		if (user_form.is_valid() and profile_form.is_valid()):
 			user_form.save()
 			profile_form.save()
-			messages.success(request, f'Profile has been changed.')
+			messages.success(request, f'Your profile has been updated.')
 			return redirect('profile_page', profile_id=profile_id)
 
 	else:
-		user_form = UserUpdateForm(request.POST, instance=request.user)
-		profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+		user_form = UserUpdateForm(instance=request.user)
+		profile_form = ProfileUpdateForm(instance=request.user.profile)
 
 	return render(request, 'users/edit_profile.html', {'user_form': user_form, 'profile_form': profile_form})
