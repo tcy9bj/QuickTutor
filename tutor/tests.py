8294c2c3f1f023ccfbfea82ac0ce797977cfadb6
Reilman79 <42817670+Reilman79@users.noreply.github.com>
2020-04-21 15:23:12 -0400

    updated tests

--- tutor/tests.py
+++ tutor/tests.py
@@ -15,24 +15,42 @@ class ViewsTest(TestCase):
 		response = self.client.get('/tutor/login/')
 		self.assertEqual(response.status_code, 200)
 
-	def test_request_view(self):
+	def test_home_view(self):
 		response = self.client.get('/tutor/home/')
-		self.assertEqual(response.status_code, 302)
+		self.assertIn(response.status_code, [200, 302])
 
-	def test_inbox_view(self):
-		response = self.client.get('/users/inbox/')
-		self.assertEqual(response.status_code, 302)
+	def test_request_view(self):
+		tutor_id = self.user1.id
+		response = self.client.get('/users/request/' + str(tutor_id) + '/')
+		self.assertIn(response.status_code, [200, 302])
+
+
+class RequestsTest(TestCase):
+	def setUp(self):
+		self.client = Client()
+		self.user1 = User.objects.create(username="tester1", email="tester1@gmail.com", first_name="John", last_name="Smith")
+		self.user2 = User.objects.create(username="tester2", email="tester2@gmail.com", first_name="Jane", last_name="Doe")
 
 	def test_request_valid(self):
-		user_request = RequestForm(data={'sender':self.user1, 'receiver':self.user2, 'course': 'CS1110', 'question': 'help me'})
+		user_request = RequestForm(data={'sender':self.user1, 'receiver':self.user2, 'course': 'CS1110', 'question': 'What is an immutable type?'})
 		self.assertTrue(user_request.is_valid())
 
-class RequestsView(TestCase):
+	def test_request_invalid(self):
+		user_request = RequestForm(data={'sender':self.user1, 'receiver':self.user2, 'course': 'CS1110', 'question': ''})
+		self.assertFalse(user_request.is_valid())
 
-	def test_requestValid(self):
-		request = RequestForm(data={'course': "CS 3240", 'question': "help me"})
-		self.assertTrue(request.is_valid())
+'''
+class ActiveTutorTest(TestCase):
+	def setUp(self):
+		self.client = Client()
+		self.user = User.objects.create(username="tester1", email="tester1@gmail.com", first_name="John", last_name="Smith")
+		self.user.profile.first_name = "Joe"
+		self.user.profile.last_name = "Smith"
+		self.user.profile.major = "Spanish"
+		self.user.profile.phone_number = "909-285-7438"
+		self.user.profile.description = "This is a test profile."
 
-	def test_requestInvalid(self):
-		request = RequestForm(data={'course': "CS 3240", 'question': ""})
-		self.assertFalse(request.is_valid())
+	def test_activate_as_tutor(self):
+		assertFalse(user.profile.active)
+		response = self.client.post()
+'''
