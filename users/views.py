c70f55e56498e8f3b07dc5e264037441eb940bd3
Anjalie Chauhan <ac3gq@virginia.edu>
2020-04-22 06:30:51 -0400

    Added to footer and created FAQ page

--- users/views.py
+++ users/views.py
@@ -149,3 +149,7 @@ def edit_profile(request, profile_id):
 		profile_form = ProfileUpdateForm(instance=request.user.profile)
 
 	return render(request, 'users/edit_profile.html', {'user_form': user_form, 'profile_form': profile_form})
+
+@login_required
+def FAQ(request):
+	return render(request, 'users/FAQ.html', {'FAQ':FAQ})
