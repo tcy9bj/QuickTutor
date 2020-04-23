9791f7b21959c6eee7156d81586a2b1d7ce98279
tcy9bj <tcy9bj@virginia.edu>
2020-04-23 17:10:12 -0400

    Update tests.py

--- users/tests.py
+++ users/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
-from users.models import Profile
-from users.forms import UserUpdateForm, ProfileUpdateForm
+from users.models import Profile, Comment
+from users.forms import UserUpdateForm, ProfileUpdateForm, FeedbackForm
 
 class UpdateFormsTestCase(TestCase):
     def setUp(self):
@@ -18,7 +18,7 @@ class UpdateFormsTestCase(TestCase):
 
     def test_ProfileUpdateForm_valid(self):
         '''Tests that the ProfileUpdateForm takes valid input'''
-        form_data = {'first_name':'Joe', 'last_name':'Smith', 'major':'Spanish', 'major2':'Foreign Affairs', 
+        form_data = {'first_name':'Joe', 'last_name':'Smith', 'major':'Spanish', 'major2':'Foreign Affairs',
                      'courses':'SPAN 1060', 'phone_number':'909-285-7438', 'description':'This is a test profile.'}
         profile_form = ProfileUpdateForm(data=form_data)
         self.assertTrue(profile_form.is_valid())
@@ -37,14 +37,14 @@ class UpdateFormsTestCase(TestCase):
         -Tests that the ProfileUpdateForm rejects invalid input
         -invalid test input: Improper phone number format
         '''
-        form_data = {'first_name':'Joe', 'last_name':'Smith', 'major':'Spanish', 'major2':'Foreign Affairs', 
+        form_data = {'first_name':'Joe', 'last_name':'Smith', 'major':'Spanish', 'major2':'Foreign Affairs',
                      'phone_number':'9092857438','description':'This is a test profile.'}
         profile_form = ProfileUpdateForm(data=form_data)
         self.assertFalse(profile_form.is_valid())
 
     def test_ProfileUpdateForm_valid_only_required_fields(self):
         '''Tests that the ProfileUpdateForm is still valid when non-required fields are left blank'''
-        form_data = {'first_name':'Joe', 'last_name':'Smith', 'major':'Spanish', 
+        form_data = {'first_name':'Joe', 'last_name':'Smith', 'major':'Spanish',
                      'phone_number':'909-285-7438'}
         profile_form = ProfileUpdateForm(data=form_data)
         self.assertTrue(profile_form.is_valid())
@@ -89,7 +89,7 @@ class ProfileTestCase(TestCase):
 
     def test_register_view_fills_out_new_user_profiles(self):
         self.assertFalse(self.user.profile.initialized)
-        post_data = {'user':self.user, 'username':'tester1', 'email':'tester1@gmail.com', 
+        post_data = {'user':self.user, 'username':'tester1', 'email':'tester1@gmail.com',
                      'first_name':'Joe','last_name':'Smith', 'major':'Spanish', 'major2':'Foreign Affairs',
                      'phone_number':'909-285-7438', 'description':'This test succeeded'}
         response = self.client.post('/users/register/', post_data, follow=True)
@@ -97,3 +97,32 @@ class ProfileTestCase(TestCase):
         profile = Profile.objects.get(id=profile_id)
         self.assertTrue(profile.initialized)
         self.assertEqual(profile.description, 'This test succeeded')
+
+class ReviewTestCases(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user1 = User.objects.create(username='tester1', email='tester1@gmail.com', first_name='John', last_name='Smith')
+        self.user2 = User.objects.create(username='tester2', email='tester2@gmail.com', first_name='Jane', last_name='Doe')
+        self.client = Client()
+
+    def test_review_valid(self):
+        user_request = FeedbackForm(data={'reviewer':self.user1, 'reviewee':self.user2, 'rating': '10', 'comment_text': 'Very helpful!'})
+        self.assertTrue(user_request.is_valid())
+
+    def test_review_invalid(self):
+        '''Test required fields'''
+        user_request = FeedbackForm(data={'reviewer':self.user1, 'reviewee':self.user2, 'rating': '10', 'comment_text': ''})
+        self.assertFalse(user_request.is_valid())
+
+    def test_review_invalid2(self):
+        user_request = FeedbackForm(data={'reviewer':self.user1, 'reviewee':self.user2, 'rating': '', 'comment_text': 'Very helpful!'})
+        self.assertFalse(user_request.is_valid())
+
+    def test_review_invalid3(self):
+        user_request = FeedbackForm(data={'reviewer':self.user1, 'reviewee':self.user2, 'rating': '', 'comment_text': ''})
+        self.assertFalse(user_request.is_valid())
+
+    def test_review_invalid4(self):
+        '''Test rating bounds'''
+        user_request = FeedbackForm(data={'reviewer':self.user1, 'reviewee':self.user2, 'rating': '11', 'comment_text': 'Very helpful!'})
+        self.assertFalse(user_request.is_valid())
